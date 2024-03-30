988b71cae50bbde7f6d848a2cd63a92397358ca2
Akhil URK23CS1055 <145592032+AKA-Akhil@users.noreply.github.com>
2024-03-30 09:59:58 +0530

    added updated values and line graph

--- ElectricityPrediction.py
+++ ElectricityPrediction.py
@@ -1,12 +1,16 @@
 import pandas as pd
+import numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_error, mean_squared_error
+import matplotlib.pyplot as plt
 
-el = pd.read_csv(r'C:\Users\akhil\Akhil\Coding\Linear Regression\Electric_Production (1).csv')
+el = pd.read_csv(r'/home/astrix/Code/Elec/Electric_Production.csv')
 
-el['DATE'] = pd.to_datetime(el['DATE'])
-el['DATE'] = el['DATE'].apply(lambda x: x.toordinal())
+el['DATE'] = pd.to_datetime(el['DATE']).apply(lambda x: x.toordinal())
+el["Value"].replace(0,np.NaN, inplace=True)
+el['Value'].fillna(el['Value'].mean(), inplace=True)
+
 X = el[['DATE']]
 y = el['Value']
 
@@ -16,5 +20,17 @@ model.fit(X_train, y_train)
 y_pred = model.predict(X_test)
 mae = mean_absolute_error(y_test, y_pred)
 
+el['Prediction'] = model.predict(X)
+el['DATE'] = el['DATE'].apply(lambda x: pd.Timestamp.fromordinal(int(x)))
+print(el)
+print(100-mae)
 
-print("Accuracy:", 100 - mae)
+plt.plot(X)
+
+plt.plot(el['DATE'], el['Value'], label='Actual')
+plt.plot(el['DATE'], el['Prediction'], label='Prediction')
+plt.xlabel('Date')
+plt.ylabel('Electricity Production')
+plt.title('Actual vs. Predicted Electricity Production')
+plt.legend()
+plt.show()
